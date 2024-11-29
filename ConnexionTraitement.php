6acbdfeb9bb455e5dcf0782e88c15e2c01589886
Touzanine Issa <i.touzanine@lprs.fr>
2024-11-29 11:32:16 +0100

    feat(base) implément des fichiers

---
+++ ConnexionTraitement.php
@@ -0,0 +1,33 @@
+<h1>Formulaire inscription</h1><br>
+<h3>Vérifier vos informations</h3>
+
+
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=ite_bibliotheque;charset=utf8','root','');
+$prenom = $bdd ->prepare('INSERT INTO inscrit(nom, prenom, email,tel_fixe ,tel_portable , ville, cp, rue) VALUES (:nom, :prenom, :email, :tel_fixe, :tel_portable, :ville, :codePostal, :rue)');
+
+
+$prenom->execute( array (
+    'prenom'=> $_POST['prenom'],
+    'nom' => $_POST['nom'],
+    'email' => $_POST['email'],
+    'tel_fixe' => $_POST['tel_fix'],
+    'tel_portable' => $_POST['tel_portable'],
+    'ville' => $_POST['ville'],
+    'codePostal' => $_POST['codePostal'],
+    'rue' => $_POST['rue'],
+));
+
+
+echo "Prénom :".$_POST['prenom']."<br/>";
+echo "nom :".$_POST['nom']."<br/>";
+echo "email :".$_POST['email']."<br/>";
+echo "tel_fix :".$_POST['tel_fix']."<br/>";
+echo "tel_portable :".$_POST['tel_portable']."<br/>";
+echo "ville :".$_POST['ville']."<br/>";
+echo "codePostal :".$_POST['codePostal']."<br/>";
+echo "rue :".$_POST['rue']."<br/>";
+?>
+
+<a href="tableau.html">Revenir sur la page de connexion</a><br>
+
