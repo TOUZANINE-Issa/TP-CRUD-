68638b59166f8f62161d2b7060ba060ae340f982
Touzanine Issa <i.touzanine@lprs.fr>
2024-12-11 09:31:28 +0100

    fix : resolution probleme creeation session

--- Session_1.php
+++ Session_1.php
@@ -2,17 +2,19 @@
 $bdd = new PDO('mysql:host=localhost;dbname=ite_bibliotheque;charset=utf8','root','');
 
 if(isset($_POST['login']) && isset($_POST['mdp'])) {
-    $mdp = $bdd->prepare('SELECT nom, prenom  WHERE nom=:login AND prenom=:mdp');
-
+    $mdp = $bdd->prepare('SELECT nom, prenom  FROM inscrit WHERE nom = :nom AND prenom = :prenom');
 
     $mdp->execute(array(
 
         'prenom' => $_POST['login'],
         'nom' => $_POST['mdp'],
     ));
+    session_start();
+    //inserer image
     echo "je suis le BOOOOOOOOOOOSSSSSSSS";
 }
 else "c'est vraiment po noce..";
+
 
 
 
