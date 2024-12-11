010c2fb6a4240e3fe7f932b66251d67a4c8217eb
Touzanine Issa <i.touzanine@lprs.fr>
2024-12-11 10:54:10 +0100

    maybe fix: probleme connexion

--- Session_1.php
+++ Session_1.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=ite_bibliotheque;charset=utf8','root','');
+$bdd = new PDO('mysql:host=localhost;dbname=ie_bibliotheque;charset=utf8','root','');
 
 if(isset($_POST['login']) && isset($_POST['mdp'])) {
     $mdp = $bdd->prepare('SELECT nom, prenom  FROM inscrit WHERE nom = :nom AND prenom = :prenom');
@@ -9,9 +9,14 @@ if(isset($_POST['login']) && isset($_POST['mdp'])) {
         'prenom' => $_POST['login'],
         'nom' => $_POST['mdp'],
     ));
+    $res = $mdp->fetch(PDO::FETCH_ASSOC);
+    var_dump($res);
+    if(empty($res)){ echo "sa marche";
+
+    }
     session_start();
     //inserer image
-    echo "je suis le BOOOOOOOOOOOSSSSSSSS";
+    echo "je suise BOOOOOOOOOOOSSSSSSSS l";
 }
 else "c'est vraiment po noce..";
 
