f1606df193b308f82d6ae04a173cd5d05247e0ba
Touzanine Issa <i.touzanine@lprs.fr>
2024-12-11 11:27:30 +0100

    fix: probleme session regler def

--- Session_1.php
+++ Session_1.php
@@ -1,25 +1,25 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=ie_bibliotheque;charset=utf8','root','');
-
-if(isset($_POST['login']) && isset($_POST['mdp'])) {
-    $mdp = $bdd->prepare('SELECT nom, prenom  FROM inscrit WHERE nom = :nom AND prenom = :prenom');
+$bdd = new PDO('mysql:host=localhost;dbname=ie_bibliotheque;charset=utf8', 'root', '');
+if (isset($_POST['login']) && isset($_POST['mdp'])) {
+    $mdp = $bdd->prepare('SELECT nom, prenom FROM inscrit WHERE prenom = :prenom AND nom = :nom');
 
     $mdp->execute(array(
-
         'prenom' => $_POST['login'],
         'nom' => $_POST['mdp'],
     ));
-    $res = $mdp->fetch(PDO::FETCH_ASSOC);
-    var_dump($res);
-    if(empty($res)){ echo "sa marche";
+    
 
+    if (empty($res)) {
+        echo "Erreur de connexion. Veuillez vérifier vos identifiants.";
+    } else {
+        session_start();
+        $_SESSION['utilisateurSession'] = $res;
+        echo "Connexion réussie. Bienvenue, " . htmlspecialchars($res['prenom']) . " !";
     }
-    session_start();
-    //inserer image
-    echo "je suise BOOOOOOOOOOOSSSSSSSS l";
+} else {
+    echo "Veuillez remplir les champs de connexion.";
 }
-else "c'est vraiment po noce..";
-
+?>
 
 
 
